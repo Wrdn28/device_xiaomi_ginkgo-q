2541604dea081425224c43b03dfb476b8c288804
Wrdn28 <ifloraxd@gmail.com>
2024-08-16 07:28:32 +0700

    Revert "ginkgo: drop IFAA/mliplay completely"
    This reverts commit b480bfd602e09a622bd97d18144ec0f61d999e55.

--- device.mk
+++ device.mk
@@ -134,6 +134,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.ginkgo
 
+# IFAA
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
