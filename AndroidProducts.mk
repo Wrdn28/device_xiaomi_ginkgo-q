1617d95245ab776f32810120b1f7df5e7e633e35
Wrdn28 <ifloraxd@gmail.com>
2024-08-27 21:10:41 +0700

    ginkgo: Qassa Bringup
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_ginkgo.mk
+    $(LOCAL_DIR)/qassa_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_ginkgo-user \
-    nad_ginkgo-userdebug \
-    nad_ginkgo-eng
+    qassa_ginkgo-user \
+    qassa_ginkgo-userdebug \
+    qassa_ginkgo-eng
