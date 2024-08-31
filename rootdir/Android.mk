c78c65e67541dddc8d7f332511ffa4ac71068f2a
Yara0202 <doodle280204@gmail.com>
2024-08-31 22:06:18 +0700

    Revert "ginkgo: configs: Force Fast Charge upto 3000mA"
    This reverts commit 17aa44425234c906215a21faa7ec83cf88e7661e.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -116,14 +116,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fastcharge.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := bin/fastcharge.sh
-LOCAL_VENDOR_MODULE    := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.performance_level.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
